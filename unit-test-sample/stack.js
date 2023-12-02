480d896095bad6d4e585c0f1803d756e8ba5f42b
lmanohara <lmanohara99@gmail.com>
2023-12-02 18:19:02 +0000

    Add stack class

---
+++ unit-test-sample/stack.js
@@ -0,0 +1,20 @@
+module.exports = class Stack {
+    constructor() {
+        this.top = -1;
+        this.items = {};
+    }
+
+    get peek() {
+        return this.items[this.top];
+    }
+
+    push(value) {
+        this.top += 1;
+        this.items[this.top] = value;
+    }
+
+    pop() {
+        this.items[this.top] = null;
+        this.top -= 1;
+    }
+}
