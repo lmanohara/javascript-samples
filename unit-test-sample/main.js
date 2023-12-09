273717bd2815759b16d8999403a201add0bef5a2
lmanohara <lmanohara99@gmail.com>
2023-12-09 15:51:53 +0000

    Add clear method input text box

--- unit-test-sample/main.js
+++ unit-test-sample/main.js
@@ -1,24 +1,22 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { Stack } from "/stack.js";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const stack = new Stack();
 
-setupCounter(document.querySelector('#counter'))
+const inputValue = document.getElementById('inputTxtBx');
+const pushBtn = document.getElementById('pushBtn');
+const popBtn = document.getElementById('popBtn');
+const peekBtn = document.getElementById('peekBtn');
+const showElements = document.getElementById('showElements');
+
+pushBtn.addEventListener('click', () => {
+    stack.push(inputValue.value);
+    inputValue.value = '';
+});
+
+popBtn.addEventListener('click', () => {
+    stack.pop();
+});
+
+peekBtn.addEventListener('click', () => {
+    showElements.value = stack.peek;
+});
